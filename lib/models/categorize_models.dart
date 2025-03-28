4430344096fec73d5c76d5f50b3f91607932fbf8
Ali Miri <100402996+4itsam@users.noreply.github.com>
2025-03-28 18:37:49 +0330

    enhance colors

--- lib/models/categorize_models.dart
+++ lib/models/categorize_models.dart
@@ -16,28 +16,28 @@ class CategoryModels {
 
     category.add(
       CategoryModels(
-        boxColor: const Color.fromARGB(82, 0, 170, 255),
+        boxColor: const Color.fromARGB(100, 0, 170, 255),
         image: "assets/images/salad.png",
         title: "Salad",
       ),
     );
     category.add(
       CategoryModels(
-        boxColor: const Color.fromARGB(82, 255, 0, 4),
+        boxColor: const Color.fromARGB(100, 255, 0, 4),
         image: "assets/images/fruit.png",
         title: "fruit",
       ),
     );
     category.add(
       CategoryModels(
-        boxColor: const Color.fromARGB(82, 0, 255, 30),
+        boxColor: const Color.fromARGB(100, 0, 255, 30),
         image: "assets/images/drinks.png",
         title: "Drinks",
       ),
     );
     category.add(
       CategoryModels(
-        boxColor: const Color.fromARGB(82, 255, 149, 0),
+        boxColor: const Color.fromARGB(100, 255, 149, 0),
         image: "assets/images/special.png",
         title: "Special",
       ),
